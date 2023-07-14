99eef4b66da901c09f7744c035a6fee937809a36
eddygulled <edgargulay69@gmail.com>
2023-07-14 15:33:11 +0300

    added preprocessing script

---
+++ preprocessing.py
@@ -0,0 +1,44 @@
+import pandas  as pd
+import numpy as np
+
+path = 'assets/'
+train  = pd.read_csv(path + 'neural_tech_swahili_sentiment.csv')
+
+train['comment'] = train['text']
+train['sentiment'] = train['labels']
+
+# Remove duplicate rows
+train = train.drop_duplicates()
+
+# Remove rows with missing values
+train = train.dropna()
+
+import re
+
+def clean_text(text):
+    # Remove special characters, punctuation, and non-Swahili characters
+    text = re.sub(r"[^a-zA-Z0-9 \-]", "", text)
+    
+    # Remove URLs
+    text = re.sub(r"http\S+|www\S+|https\S+", "", text)
+    # Remove HTML tags (if available)
+    text = re.sub(r"<.*?>", "", text)
+    # Normalize whitespace
+    text = re.sub(r"\s+", " ", text).strip()
+    # remove numbers
+    text = re.sub(r'\b\d+(?:\.\d+)?\s+', '', text) 
+    text = re.sub(r"\d", "", text)
+    # set in lowercase
+    text = text.lower()   
+    
+    # Remove consecutive duplicate characters (e.g., 'loooove' to 'love')
+    text = re.sub(r"(.)\1+", r"\1", text)
+    
+    return text
+
+# Apply text cleaning to the 'text' column
+train['comment'] = train['comment'].apply(clean_text)
+
+train.drop(['id', 'text', 'labels'], axis=1)
+
+train.to_csv(path + 'cleaned_training_set.csv', index=False)
